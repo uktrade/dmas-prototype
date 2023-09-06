5cef663d16700aef52cfac1ac7163876e1012e82
Jonathan Porton <58692841+J-Porton@users.noreply.github.com>
2023-09-06 10:53:43 +0100

    More updates and work around MAU

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,8 +1,29 @@
 const sampleBarrierData = require('./barrier-information/dbt-barrier-sample-data');
 
+// =======================
+// Set some default values
+// =======================
+
+let myDefaults = {}
+
+myDefaults.myNewVar = "My new var value"
+
+
+
+// Default approvers list
+defaultApprovers = [
+  "Department for Environment Food & Rural Affairs",
+  "HM Revenue & Customs",
+  "Market Access Regional Coordinators",
+  "BTR Regional Teams"
+]
+
+
 module.exports = {
 
-  sampleBarrierData //populates barrier information and public tabs
+  sampleBarrierData, //populates barrier information and public tabs
+  myDefaults,
+  defaultApprovers
 
 }
 
